2cfbaa8bc84aec9e1c2313b521b7ae05bcada959
Leah <leah.grace555@gmail.com>
2020-06-08 19:19:26 -0400

    testing

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ app.get('*', (request,response) => {
   response.send('oops.. something went wrong')
 })
 
+function NewLocation(searchQuery, obj) {
+  this.searchQuery = searchQuery;
+  this.formattedQuery = obj.display_name;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+
+}
+
+app.get('/location', (request,response) => {
+  try {
+    let searchQuery = request.query.city;
+    let geoData = require('./data/location.json');
+    let returnObj = new NewLocation(searchQuery, geoData[0]);
+  } catch(err){
+    response.status(500).send('whoops. Something went wrong');
+  }
+})
