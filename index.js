d9d126b5f4f7ba9839942541a1c119b02e011e69
Leah <leah.grace555@gmail.com>
2020-06-09 21:03:39 -0400

    replaced weather.json with weatherbit api

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 const express = require('express');
 const superagent = require('superagent');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
-
-const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT;
@@ -19,9 +18,7 @@ function NewLocation(searchQuery, obj) {
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
-
 }
-
 
 app.get('/location', (request,response) => {
   let searchQuery = request.query.city;
@@ -40,19 +37,42 @@ function Weather(obj) {
 }
 
 app.get('/weather', (request,response) => {
-  try {
-    let weatherData = require('./data/weather.json');
-    let weatherDays = weatherData.data.map( weatherDay => {
-      let day = new Weather(weatherDay);
-      console.log(day);
-      return day;
-    })
-    console.log(weatherDays)
-    response.status(200).send(weatherDays);
-  } catch(err){
-    response.status(500).send('storm clouds a coming cuz we did something wrong')
-  }
+  let searchQuery = request.query.search_query;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${process.env.WEATHER_DATA}&days=8`
+
+  superagent.get(url)
+    .then(resultsFromSuperAgent => {
+      //do something
+      console.log(resultsFromSuperAgent.body.data);
+      let weatherDays = resultsFromSuperAgent.body.data.map( weatherDay => {
+        let day = new Weather(weatherDay);
+        console.log(day);
+        return day;
+      })
+      console.log(weatherDays)
+      response.status(200).send(weatherDays);
+    }).catch(err => console.log(err));
+
+  //let weatherData = require('./data/weather.json');
+  // catch(err){
+  //   response.status(500).send('storm clouds a coming cuz we did something wrong')
+  // }
 })
+
+// app.get('/weather', (request,response) => {
+//   try {
+//     let weatherData = require('./data/weather.json');
+//     let weatherDays = weatherData.data.map( weatherDay => {
+//       let day = new Weather(weatherDay);
+//       console.log(day);
+//       return day;
+//     })
+//     console.log(weatherDays)
+//     response.status(200).send(weatherDays);
+//   } catch(err){
+//     response.status(500).send('storm clouds a coming cuz we did something wrong')
+//   }
+// })
 
 //error handling:
 app.get('*', (request,response) => {
